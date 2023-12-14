da195e0bfe7099e69b0299483381666dd40e8b14
SemihAcar7 <smhacr7@gmail.com>
2023-12-14 21:54:31 +0300

    düzenlendi

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {FaRegThumbsUp,FaRegTrashAlt} from 'react-icons/fa'
+
+function Tasks({list,setList}) {
+
+    function deleteItem(index){
+        let newList = [...list]
+        newList.splice(index,1)
+        setList(newList)
+    }
+
+
+
+    let ffalse = "flex justify-between p-4 items-center rounded bg-transparent"
+    let ttrue = "flex justify-between p-4 itemc-center rounded bg-zinc-600"
+  return (
+    <div className='w-1/2 mx-auto'>
+        <ul className='unstyled mt-3'>
+            {list.map((item,index) => (
+                <li key={index} className={item.isCompleted ? ttrue :ffalse}>
+                    <span className='text-3xl'>{item.text}</span>
+                    <div className='flex gap-3'>
+                        <FaRegThumbsUp size = {35} className={"cursor-pointer hover:text-green-500"}/>
+                        <FaRegTrashAlt size = {35} className={"cursor-pointer hover:text-red-500"} onClick={() => deleteItem(index) }/>
+                    </div>
+
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Tasks
